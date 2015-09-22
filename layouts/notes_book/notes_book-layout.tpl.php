5a56271fa0cc19f98fa5b179d8aadc88826c5193
Hein Bekker <hein@netbek.co.za>
2015-09-22 10:47:54 +0200

    Bulk update

--- layouts/notes_book/notes_book-layout.tpl.php
+++ layouts/notes_book/notes_book-layout.tpl.php
@@ -1,5 +1,7 @@
 <div>
   <?php print render($page['notes_book_title']); ?>
+  <?php print render($page['notes_book_toc']); ?>
   <?php print render($page['notes_book_body']); ?>
+  <?php print render($page['notes_book_nav']); ?>
   <?php print render($page['notes_book_updated']); ?>
 </div>
