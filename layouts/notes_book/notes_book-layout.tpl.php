db663bd1ba9d7495cd321f3e5a07b43a5ca3c236
Hein Bekker <hein@netbek.co.za>
2015-10-18 17:47:56 +0200

    Add contribute banner

--- layouts/notes_book/notes_book-layout.tpl.php
+++ layouts/notes_book/notes_book-layout.tpl.php
@@ -3,6 +3,7 @@
   <?php print render($page['notes_book_toc']); ?>
   <?php print render($page['notes_book_body']); ?>
   <?php print render($page['notes_book_nav']); ?>
+  <?php print render($page['notes_book_contribute']); ?>
   <?php print render($page['notes_book_updated']); ?>
   <?php print render($page['notes_book_comments']); ?>
 </div>
